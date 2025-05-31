28180cf8e2663ff116d4a675d0040e89ba207f4a
Anwesa Ghosh <anwesa2021@gmail.com>
2025-05-31 23:18:58 +0530

    added the pacjage

---
+++ triptide/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "../Components/Helper/Navbar";
+
+const HomePage = () => {
+  return (
+    <div className='overflow-x-hidden'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default HomePage;
